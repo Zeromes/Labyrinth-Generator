74a03abe21d53fa609dffa9d7c26dffa0ab1df7f
Zeromes <178800633@qq.com>
2019-06-28 20:52:54 +0800

    Create Project

---
+++ LabyrinthGenerator/LabyrinthGenerator/LabyrinthGeneratorView.cpp
@@ -0,0 +1,105 @@
+﻿
+// LabyrinthGeneratorView.cpp: CLabyrinthGeneratorView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "LabyrinthGenerator.h"
+#endif
+
+#include "LabyrinthGeneratorDoc.h"
+#include "LabyrinthGeneratorView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CLabyrinthGeneratorView
+
+IMPLEMENT_DYNCREATE(CLabyrinthGeneratorView, CView)
+
+BEGIN_MESSAGE_MAP(CLabyrinthGeneratorView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CLabyrinthGeneratorView 构造/析构
+
+CLabyrinthGeneratorView::CLabyrinthGeneratorView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CLabyrinthGeneratorView::~CLabyrinthGeneratorView()
+{
+}
+
+BOOL CLabyrinthGeneratorView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CLabyrinthGeneratorView 绘图
+
+void CLabyrinthGeneratorView::OnDraw(CDC* /*pDC*/)
+{
+	CLabyrinthGeneratorDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CLabyrinthGeneratorView 打印
+
+BOOL CLabyrinthGeneratorView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CLabyrinthGeneratorView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CLabyrinthGeneratorView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CLabyrinthGeneratorView 诊断
+
+#ifdef _DEBUG
+void CLabyrinthGeneratorView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CLabyrinthGeneratorView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CLabyrinthGeneratorDoc* CLabyrinthGeneratorView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CLabyrinthGeneratorDoc)));
+	return (CLabyrinthGeneratorDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CLabyrinthGeneratorView 消息处理程序
